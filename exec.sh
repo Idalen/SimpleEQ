a803f2e5318128bf9930206a5adbceaa4eb2463d
idalen <danielmartins@usp.br>
2022-07-10 13:22:00 -0300

    initial layout

--- exec.sh
+++ exec.sh
@@ -1,10 +1,15 @@
 #! /bin/bash
 
 case $1 in
-    -b) 
+    -b|--build) 
     echo "Building application"
-    cd Builds/LinuxMakefile && make clean && make
+    cd Builds/LinuxMakefile && make clean && make && cd ../../
     echo "Built!"
+    ;;
+    -h|--help)
+    echo "Exec this file to run SimpleEQ."
+    echo "Use [-b | --build] to compile the program before execution." 
+    exit 0
     ;;
 esac
 
